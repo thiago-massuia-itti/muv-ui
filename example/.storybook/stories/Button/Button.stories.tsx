7484660e5599a862ff82f2961fa298e93760c1a3
thiago-massuia-itti <thiago.massuia@itti.digital>
2024-06-21 17:00:15 -0300

    review

--- example/.storybook/stories/Button/Button.stories.tsx
+++ example/.storybook/stories/Button/Button.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, ComponentStory } from '@storybook/react';
-import { MuvButton, VARIANTS } from '../../../components/molecules/MuvButton';
+import {MuvButton} from 'muv-ui';
 import React from 'react';
 const iconCorp = require('assets/images/icon-briefcase-selected.png');
 
@@ -17,31 +17,31 @@ const Template: ComponentStory<typeof MuvButton> = (args) => <MuvButton {...args
 export const Filled = Template.bind({});
 Filled.args = {
   label: 'Filled Button',
-  variant: VARIANTS.FILLED,
+  // variant: VARIANTS.FILLED,
 };
 
 export const Outline = Template.bind({});
 Outline.args = {
   label: 'Outline Button',
-  variant: VARIANTS.OUTLINE,
+  // variant: VARIANTS.OUTLINE,
 };
 
 export const Disabled = Template.bind({});
 Disabled.args = {
   label: 'Disabled Button',
-  variant: VARIANTS.FILLED,
+  // variant: VARIANTS.FILLED,
   disabled: true,
 };
 
 export const WithIconLeft = Template.bind({});
 WithIconLeft.args = {
   label: 'Button with Icon',
-  variant: VARIANTS.FILLED,
+  // variant: VARIANTS.FILLED,
   iconLeft: iconCorp, // replace with actual path to icon
 };
 export const WithIconRight = Template.bind({});
 WithIconRight.args = {
   label: 'Button with Icon',
-  variant: VARIANTS.FILLED,
+  // variant: VARIANTS.FILLED,
   iconRight: iconCorp, // replace with actual path to icon
 };
